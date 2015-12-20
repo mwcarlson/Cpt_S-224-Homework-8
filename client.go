6001e58b39e655fa3f12fa6df512b1d8be7d76ee
mwcarlson <michael-carlson@hotmail.com>
2015-12-20 04:22:30 -0800

    Create client.go

---
+++ client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io"
+	"net"
+	"os"
+)
+
+func handleConnection(c net.Conn) 
+{
+	// Copy up to 128 bytes from the connection to the screen.
+	io.CopyN(os.Stdout, c, 128)
+}
+
+func main() 
+{
+	// listen for connections on port 5555
+	list, err := net.Listen("tcp", ":5555")
+	if err != nil 
+	{
+		// If others are using the same computer
+		// you might want to change the port
+		panic(err)
+	}
+
+	// loop forever!
+	for 
+	{
+		con, err := list.Accept()
+		if err != nil 
+		{
+			panic(err)
+		}
+
+		go handleConnection(con)
+	}
+}

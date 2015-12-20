f82f996010a56a4053860c9fcbcf936c91372714
mwcarlson <michael-carlson@hotmail.com>
2015-12-20 04:21:23 -0800

    Create server.go

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import 
+(
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"time"
+)
+
+func handleConnection(c net.Conn) 
+{
+	// Copy up to 128 bytes from the connection to the screen.
+	io.CopyN(os.Stdout, c, 128)
+	fmt.Fprintf(c, "The time is %s\n", time.Now())
+}
+
+func main() 
+{
+	
+}

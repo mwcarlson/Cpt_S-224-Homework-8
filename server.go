267a017cab9183a0bb990ae7653ed98e96b1a299
mwcarlson <michael-carlson@hotmail.com>
2015-12-20 23:40:20 -0800

    Update server.go

--- server.go
+++ server.go
@@ -5,38 +5,34 @@ import
 	"io"
 	"net"
 	"os"
+	"fmt"
+	"time"
 )
 
-func handleConnection(c net.Conn) 
-{
+func handleConnection(c net.Conn) {
 	// copy up to 128 bytes from the connection to the screen
 	io.CopyN(os.Stdout, c, 128)
 	// print out client's IP address
-	fmt.Fprintf(con, "%s\n", con.RemoteAddr().String())
+	fmt.Fprintf(c, "%s\n", c.RemoteAddr().String())
 	// print out the current time every second
-	for range time.Tick(time.Second)
-	{
-		fmt.Fprintf(con, "%s\n", time.Now().String())
+	for range time.Tick(time.Second) {
+		fmt.Fprintf(c, "%s\n", time.Now().String())
 	}
 }
 
-func main() 
-{
+func main() {
 	// listen for connections on port 5555
 	list, err := net.Listen("tcp", ":7778")
-	if err != nil 
-	{
+	if err != nil {
 		// if others are using the same computer
 		// you might want to change the port
 		panic(err)
 	}
 
 	// loop forever!
-	for 
-	{
+	for {
 		con, err := list.Accept()
-		if err != nil 
-		{
+		if err != nil {
 			panic(err)
 		}
 

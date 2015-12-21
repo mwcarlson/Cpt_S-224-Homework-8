0af02f48dced2956fdd58ee186c8249dd73360a3
mwcarlson <michael-carlson@hotmail.com>
2015-12-20 20:39:20 -0800

    Update client.go

--- client.go
+++ client.go
@@ -1,37 +1,24 @@
 package main
 
-import (
-	"io"
+import 
+(
+	"fmt"
 	"net"
-	"os"
 )
-
-func handleConnection(c net.Conn) 
-{
-	// Copy up to 128 bytes from the connection to the screen.
-	io.CopyN(os.Stdout, c, 128)
-}
 
 func main() 
 {
-	// listen for connections on port 5555
-	list, err := net.Listen("tcp", "localhost:7778")
+	// connect to 'localhost' on port '7778'
+	con, err := net.Dial("tcp", "localhost:7778")
 	if err != nil 
 	{
-		// If others are using the same computer
-		// you might want to change the port
 		panic(err)
 	}
 
-	// loop forever!
-	for 
-	{
-		con, err := list.Accept()
-		if err != nil 
-		{
-			panic(err)
-		}
+	// Write a message
+	fmt.Fprintf(con, "Hello Internet!")
 
-		go handleConnection(con)
-	}
+	// Close the connection
+	con.Close()
 }
+
